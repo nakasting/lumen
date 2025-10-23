eca270bf6bac2d56721216fb32b6d240aec9736e
Sting <bobadillasting@gmail.com>
2025-10-22 22:29:06 -0300

    Update: Refactor function CreateGenre

--- internal/handler/genre_handler.go
+++ internal/handler/genre_handler.go
@@ -6,22 +6,20 @@ import (
 	"lumen/internal/dto"
 	"lumen/internal/service"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
 )
 
-type Genrehandler struct {
+type GenreHandler struct {
 	service   service.GenreService
 	validator *validator.Validate
 	logger    *zap.Logger
 }
 
-func NewGenreHandler(s service.GenreService, v *validator.Validate, l *zap.Logger) *Genrehandler {
-	return &Genrehandler{
+func NewGenreHandler(s service.GenreService, v *validator.Validate, l *zap.Logger) *GenreHandler {
+	return &GenreHandler{
 		service:   s,
 		validator: v,
 		logger:    l,
@@ -29,167 +27,75 @@ func NewGenreHandler(s service.GenreService, v *validator.Validate, l *zap.Logge
 }
 
 // Routes
-func (h *Genrehandler) RegisterRoutes(r chi.Router) {
-	r.Get("/", h.GetGenres)
+func (h *GenreHandler) RegisterRoutes(r chi.Router) {
 	r.Post("/", h.CreateGenre)
-	r.Put("/{id}", h.UpdateGenre)
 }
 
 /* ###################################################################################### */
-func (h *Genrehandler) GetGenres(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+func (h *GenreHandler) CreateGenre(w http.ResponseWriter, r *http.Request) {
+	var genreRequest dto.GenreReq
+	dec := json.NewDecoder(r.Body)
+	enc := json.NewEncoder(w)
 
-	genres, err := h.service.FindAll()
-
-	if err != nil {
-		h.logger.Error("Cannot get genres on database", zap.Error(err))
+	if err := dec.Decode(&genreRequest); err != nil {
+		h.logger.Error("Error encoding json", zap.Error(err))
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "body",
-					Error: "Error internal server",
-				},
-			},
-		})
+		errorResponse := dto.NewErrorResponse("Error internal server")
+		enc.Encode(errorResponse)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(genres)
+	if err := h.validator.Struct(genreRequest); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
 
-}
+		var fieldErrors []dto.FieldError
+		validationErrors := err.(validator.ValidationErrors)
 
-/* ###################################################################################### */
-func (h *Genrehandler) CreateGenre(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var genreReq dto.GenreReq
-
-	if err := json.NewDecoder(r.Body).Decode(&genreReq); err != nil {
-		h.logger.Warn("Error decode request", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorResponse{Message: "Error internal server"})
-		return
-	}
-
-	if err := h.validator.Struct(genreReq); err != nil {
-		h.logger.Warn("Error validate model", zap.Error(err))
-		errs := err.(validator.ValidationErrors)
-		var errFields []dto.ErrorField
-		for _, e := range errs {
-			errFields = append(errFields, dto.ErrorField{
-				Field: strings.ToLower(e.Field()),
-				Error: e.Error(),
-			})
+		for _, validationError := range validationErrors {
+			fieldError := dto.NewFieldError(validationError.Field(), validationError.Error())
+			fieldErrors = append(fieldErrors, *fieldError)
 		}
-
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: errFields,
-		})
+		errorRes := dto.NewValidationErrors(fieldErrors)
+		enc.Encode(&errorRes)
 		return
 	}
 
-	exists, err := h.service.Exists(&genreReq, 0)
+	genreExists, err := h.service.Exists(&genreRequest, 0)
 
 	if err != nil {
-		h.logger.Error("Cannot validate genre exists", zap.Error(err))
+		h.logger.Error("Error checking on database", zap.Error(err))
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "body",
-					Error: "Error internal server",
-				},
-			},
-		})
-	}
-
-	if exists {
-		h.logger.Warn("Genre already exists", zap.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "name",
-					Error: fmt.Sprintf("Genre with name %s is already exists", genreReq.Name),
-				},
-			},
-		})
+		errorResponse := dto.NewErrorResponse("Error internal server")
+		enc.Encode(errorResponse)
 		return
 	}
 
-	res, err := h.service.Create(&genreReq)
+	if genreExists {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		var fieldErrors []dto.FieldError
+		fieldError := dto.NewFieldError("Name", fmt.Sprintf("Genre with name '%s' is already exists", genreRequest.Name))
+		fieldErrors = append(fieldErrors, *fieldError)
+		errorRes := dto.NewValidationErrors(fieldErrors)
+		enc.Encode(&errorRes)
+		return
+	}
+
+	genreResponse, err := h.service.Create(&genreRequest)
 
 	if err != nil {
-		h.logger.Error("Cannot save model", zap.Error(err))
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "body",
-					Error: "Error internal server",
-				},
-			},
-		})
+		errorResponse := dto.NewErrorResponse("Error internal server")
+		enc.Encode(&errorResponse)
 		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(res)
-}
-
-/* ###################################################################################### */
-func (h *Genrehandler) UpdateGenre(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	id := chi.URLParam(r, "id")
-
-	var genreReq dto.GenreReq
-
-	if err := json.NewDecoder(r.Body).Decode(&genreReq); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "body",
-					Error: "Cannot decode request",
-				},
-			},
-		})
-		return
-	}
-
-	genreId, err := strconv.Atoi(id)
-	if err != nil {
-		h.logger.Error("Invalid param", zap.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "url",
-					Error: "Invalid param",
-				},
-			},
-		})
-		return
-	}
-
-	genreRes, err := h.service.Update(uint(genreId), &genreReq)
-
-	if err != nil {
-		h.logger.Warn("Cannot update model", zap.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorRes{
-			Errors: []dto.ErrorField{
-				dto.ErrorField{
-					Field: "body",
-					Error: "Error internal server",
-				},
-			},
-		})
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(genreRes)
+	w.WriteHeader(http.StatusCreated)
+	enc.Encode(genreResponse)
 }
